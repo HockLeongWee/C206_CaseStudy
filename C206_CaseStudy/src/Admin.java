e52b7ff2c76105d989ec2e88aee8f34f3431495e
HockLeongWee <83415155+HockLeongWee@users.noreply.github.com>
2021-08-07 00:31:02 +0800

    Update Admin.java

--- C206_CaseStudy/src/Admin.java
+++ C206_CaseStudy/src/Admin.java
@@ -1,4 +1,14 @@
-//Hello
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Izzat Zahirin
+ * Student ID: 20042321
+ * Class: E37E
+ * Date/Time created: Monday 02-08-2021 21:28
+ */
+
 public class Admin {
 	private String name;
 	private String password;
@@ -30,6 +40,8 @@ public class Admin {
 	public String getAdminId() {
 		return adminId;
 	}
-}
 
-	
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+}
