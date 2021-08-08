2a1439f4fe772f5661e7a0eefaaa319a6aaa4688
HockLeongWee <83415155+HockLeongWee@users.noreply.github.com>
2021-08-09 02:25:54 +0800

    Updated student class

--- C206_CaseStudy/src/student.java
+++ C206_CaseStudy/src/student.java
@@ -1,5 +1,3 @@
-
-
 public class student {
 	
 	private int id;
@@ -7,7 +5,18 @@ public class student {
 	private String grade;
 	private String className;
 	private String classroomTeacher;
+	private String registrationId;
 	
+	public student(int id, String name, String grade, String className, String classroomTeacher, String registrationId) {
+		
+	this.id = id;
+	this.name = name;
+	this.grade = grade;
+	this.className = className;
+	this.classroomTeacher = classroomTeacher;
+	this.registrationId = registrationId;
+	
+	}
 	
 	public student(int id, String name, String grade, String className, String classroomTeacher) {
 		
@@ -16,35 +25,56 @@ public class student {
 	this.grade = grade;
 	this.className = className;
 	this.classroomTeacher = classroomTeacher;
+	
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getGrade() {
 		return grade;
 	}
+	
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
+	
 	public String getClassName() {
 		return className;
 	}
+	
 	public void setClassName(String className) {
 		this.className = className;
 	}
+	
 	public String getClassroomTeacher() {
 		return classroomTeacher;
 	}
+	
 	public void setClassroomTeacher(String classroomTeacher) {
 		this.classroomTeacher = classroomTeacher;
 	}
+	public String getRegistrationId() {
+        return registrationId;
+    }
+	
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+	
 }
+
+
